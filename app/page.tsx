7dc4b3ebb65de8193005aeea2c823125835f454d
Mina Hussain <saminahu@gmail.com>
2024-07-17 17:20:03 -0700

    add responsive flex design

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,20 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="flex flex-col fixed bottom-0 left-0 flex w-full justify-start bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <h1 className="font-serif text-3xl w-20 h-full">Mina Hussain</h1>
-          <p className="font-sans">
-            Frontend engineer and designer in the bay area
-          </p>
-          <p className="font-sans">
-            Leading with curiosity at{' '}
-            <a href="https://attnhussain.com/" className="font-bold">
-              attnhussain
-            </a>
-          </p>
-          <p className="font-sans">Operating on medium roast</p>
+    <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-8">
+      <div className="flex z-10 w-full items-start flex-col-reverse justify-between sm:flex-row">
+        <div className="flex flex-col w-full lg:static lg:size-auto">
+          <h1 className="font-serif text-2xl w-20 h-full">Mina Hussain</h1>
+          <div className="text-sm font-sans">
+            <p>Frontend engineer and designer in the bay area</p>
+            <p>
+              Leading with curiosity at{' '}
+              <a href="https://attnhussain.com/" className="font-bold">
+                attnhussain
+              </a>
+            </p>
+            <p>Operating on medium roast</p>
+          </div>
         </div>
         <div>
           <Image
@@ -31,18 +31,16 @@ export default function Home() {
       </div>
 
       <div className="relative flex overflow-x-hidden w-full">
-        <div className="flex justify-start items-start">
-          <div className="relative w-[491.83px] h-[240px] rounded-md overflow-hidden">
-            <a href="https://github.com/minahussain/homage" target="_blank">
-              <div className="absolute inset-0 bg-orange-800/50 w-full h-full z-10 hover:bg-orange-800/40 transition-colors ease-in delay-150" />
-              <Image
-                src="/homage-app.png"
-                alt="Homage App Screenshot"
-                fill
-                style={{ objectFit: 'cover' }}
-              />
-            </a>
-          </div>
+        <div className="relative w-[491.83px] h-[240px] rounded-md overflow-hidden">
+          <a href="https://github.com/minahussain/homage" target="_blank">
+            <div className="absolute inset-0 bg-orange-800/50 w-full h-full z-10 hover:bg-orange-800/40 transition-colors ease-in delay-150" />
+            <Image
+              src="/homage-app.png"
+              alt="Homage App Screenshot"
+              fill
+              style={{ objectFit: 'cover' }}
+            />
+          </a>
         </div>
       </div>
     </main>
